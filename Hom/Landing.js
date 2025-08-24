ae3efaa2d14eb89a00e37e0cd2a105409b2627f6
Arthittaya061 <arthittayaploy8202@gmail.com>
2025-08-24 20:49:27 +0700

    first commit

---
+++ Hom/Landing.js
@@ -0,0 +1,7 @@
+document.querySelector('.login-button').addEventListener('click', () => {
+    window.location.href = 'login.html'; // This will send the user to login.html
+});
+
+document.querySelector('.signup-button').addEventListener('click', () => {
+    window.location.href = 'SignUp.html'; // This will send the user to SignUp.html
+});
